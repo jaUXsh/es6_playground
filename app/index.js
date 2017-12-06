41d61fc16fda1abe5645c5db5751d5c61ee44747
jauxsh <joshua.jih.chang@gmail.com>
2017-12-06 12:55:15 -0800

    made calc.js and imported from index.js

--- app/index.js
+++ app/index.js
@@ -1,1 +1,12 @@
-console.log('App working: open app/index.js to start exploring ES6');
+import * as math from "./calc";
+
+console.log("math: ", math);
+
+//import addStuff, {subtract, multiply} from './calc';
+
+console.log("5 + 7 =", math.default(5, 7));
+
+console.log("8 - 9 =", math.subtract(8,9));
+
+console.log("134 * 23525 =", math.multiply(134, 23525));
+
